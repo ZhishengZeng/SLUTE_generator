c7c486017c86a309224ac26bec0c1375b033348f
ZhishengZeng <879849680@qq.com>
2021-03-13 10:54:15 +0800

    update

--- generateSLUT.cpp
+++ generateSLUT.cpp
@@ -7,7 +7,7 @@ int main()
 {
   std::ofstream* slut_file = new std::ofstream("slut.txt");
 
-  for (size_t i = 2; i <= 9; i++) {
+  for (size_t i = 2; i <= 6; i++) {
     std::cout << "[INFO] Now create " << i << " points look-up table" << std::endl;
     double start, end;
     start = Util::microtime();
@@ -16,7 +16,7 @@ int main()
     generator.set_point_num(i);
     generator.set_slut_file(slut_file);
     generator.init();
-    // generator.process();
+    generator.process();
 
     end = Util::microtime();
     std::cout << "[INFO] Created " << i << " points look-up table : " << (end - start) << "s"
